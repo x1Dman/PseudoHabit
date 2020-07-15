6d065336b33b9a1e01c95c3b657b24b1ab0ef4c7
Khusnutdinov Nikita <glaider102@mail.ru>
2020-07-15 17:32:31 +0300

    upd 1.3

---
+++ SberTestApp/ViewControllers/HabitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HabitViewController.swift
+//  SberTestApp
+//
+//  Created by Никита Хуснутдинов on 7/15/20.
+//  Copyright © 2020 Никита Хуснутдинов. All rights reserved.
+//
+
+import UIKit
+
+class HabitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
